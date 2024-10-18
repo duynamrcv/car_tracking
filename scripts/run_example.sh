adc5fb966d9ea0f394b58e512d77571508e9603f
Duy-Nam Bui <duynam.robotics@gmail.com>
2024-10-18 09:40:26 +0700

    Add scripts

--- scripts/run_example.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-./scripts/build.sh || { echo '*********************** Build failed! ***********************' ; exit 1; }
-
-export LD_LIBRARY_PATH=$(pwd)/vendor/opencv/lib
-./build/debug/bin/example 1 data/basement1.txt data/trajectory1.csv
